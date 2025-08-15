a3b94972ce2b6733618c5365cc19c802725bfbf5
matheusspacifico <matheuspacifico44@gmail.com>
2025-08-15 19:22:26 -0300

    feat: add standard methods and placeholder values

--- src/main/java/com/matheusspacifico/Megabool.java
+++ src/main/java/com/matheusspacifico/Megabool.java
@@ -2,56 +2,96 @@ package com.matheusspacifico;
 
 public enum Megabool {
     // for the true ones
-    TRUE,
-    DOUBLE_TRUE,
-    BIG_IF_TRUE,
-    TRUE_ON_MY_COMPUTER,
-    TRUEISH,
-    TRUE_BUT_IRRELEVANT,
-    HARDCODED_TRUE,
-    MEGATRUE,
-    GASLIT_INTO_TRUE,
-    TRUE_ON_A_GOOD_DAY,
-    SOMETIMES_TRUE,
-    FACTS,
-    I_MEAN_IT_IS_TRUE,
-    KINDA_TRUE,
-    PROBABLY_TRUE_I_COULD_BE_WRONG_THO,
-    TRUE_IN_BRAZIL,
+    TRUE(Category.AFFIRMATIVE, 1),
+    DOUBLE_TRUE(Category.AFFIRMATIVE, 1),
+    BIG_IF_TRUE(Category.AFFIRMATIVE, 1),
+    TRUE_ON_MY_COMPUTER(Category.AFFIRMATIVE, 1),
+    TRUEISH(Category.AFFIRMATIVE, 1),
+    TRUE_BUT_IRRELEVANT(Category.AFFIRMATIVE, 1),
+    HARDCODED_TRUE(Category.AFFIRMATIVE, 1),
+    MEGATRUE(Category.AFFIRMATIVE, 1),
+    GASLIT_INTO_TRUE(Category.AFFIRMATIVE, 1),
+    TRUE_ON_A_GOOD_DAY(Category.AFFIRMATIVE, 1),
+    SOMETIMES_TRUE(Category.AFFIRMATIVE, 1),
+    FACTS(Category.AFFIRMATIVE, 1),
+    I_MEAN_IT_IS_TRUE(Category.AFFIRMATIVE, 1),
+    KINDA_TRUE(Category.AFFIRMATIVE, 1),
+    PROBABLY_TRUE_I_COULD_BE_WRONG_THO(Category.AFFIRMATIVE, 1),
+    TRUE_IN_BRAZIL(Category.AFFIRMATIVE, 1),
 
     // for the false ones
-    FALSE,
-    DOUBLE_FALSE,
-    BIG_IF_FALSE,
-    HELL_NAH_NAH,
-    SKILL_ISSUE,
-    FALSEISH,
-    WISH_IT_WAS_TRUE,
-    ITS_FALSE_LITTLE_BRO,
-    BRO_THOUGHT_IT_WAS_TRUE,
-    HARDCODED_FALSE,
-    MEGAFALSE,
-    GASLIT_INTO_FALSE,
-    FALSE_ON_A_GOOD_DAY,
-    SOMETIMES_FALSE,
-    I_MEAN_IT_IS_FALSE,
-    KINDA_FALSE,
-    PROBABLY_FALSE_I_COULD_BE_WRONG_THO,
+    FALSE(Category.NEGATIVE, 1),
+    DOUBLE_FALSE(Category.NEGATIVE, 1),
+    BIG_IF_FALSE(Category.NEGATIVE, 1),
+    HELL_NAH_NAH(Category.NEGATIVE, 1),
+    SKILL_ISSUE(Category.NEGATIVE, 1),
+    FALSEISH(Category.NEGATIVE, 1),
+    WISH_IT_WAS_TRUE(Category.NEGATIVE, 1),
+    ITS_FALSE_LITTLE_BRO(Category.NEGATIVE, 1),
+    BRO_THOUGHT_IT_WAS_TRUE(Category.NEGATIVE, 1),
+    HARDCODED_FALSE(Category.NEGATIVE, 1),
+    MEGAFALSE(Category.NEGATIVE, 1),
+    GASLIT_INTO_FALSE(Category.NEGATIVE, 1),
+    FALSE_ON_A_GOOD_DAY(Category.NEGATIVE, 1),
+    SOMETIMES_FALSE(Category.NEGATIVE, 1),
+    I_MEAN_IT_IS_FALSE(Category.NEGATIVE, 1),
+    KINDA_FALSE(Category.NEGATIVE, 1),
+    PROBABLY_FALSE_I_COULD_BE_WRONG_THO(Category.NEGATIVE, 1),
 
     // for the uncertain ones
-    MAYBE,
-    PERHAPS,
-    BIG_MAYBE,
-    IT_DEPENDS,
-    ITS_COMPLICATED,
-    IDK_BRO,
-    DONT_CARE,
+    MAYBE(Category.UNCERTAIN, 1),
+    PERHAPS(Category.UNCERTAIN, 1),
+    BIG_MAYBE(Category.UNCERTAIN, 1),
+    IT_DEPENDS(Category.UNCERTAIN, 1),
+    ITS_COMPLICATED(Category.UNCERTAIN, 1),
+    IDK_BRO(Category.UNCERTAIN, 1),
+    DONT_CARE(Category.UNCERTAIN, 1),
 
     // for the quantum state of mind ones
-    BOTH,
-    SCHRODINGER_BOOL,
-    OSCILLATING,
-    YOU_DONT_WANNA_KNOW_THIS_ONE_BUDDY,
-    RAGEQUIT,
-    JUST_PUSH_TO_MAIN_ALREADY,
+    BOTH(Category.CHAOTIC, 1),
+    SCHRODINGER_BOOL(Category.CHAOTIC, 1),
+    OSCILLATING(Category.CHAOTIC, 1),
+    YOU_DONT_WANNA_KNOW_THIS_ONE_BUDDY(Category.CHAOTIC, 1),
+    RAGEQUIT(Category.CHAOTIC, 1),
+    JUST_PUSH_TO_MAIN_ALREADY(Category.CHAOTIC, 1);
+
+    private final Category category;
+    private final int power;
+
+    Megabool(Category category, int power) {
+        this.category = category;
+        this.power = power;
+    }
+
+    public Megabool resolve() {
+        return this;
+    }
+
+    public boolean isAffirmative() {
+        return this.category == Category.AFFIRMATIVE;
+    }
+
+    public boolean isNegative() {
+        return this.category == Category.NEGATIVE;
+    }
+
+    public boolean isUncertain() {
+        return this.category == Category.UNCERTAIN;
+    }
+
+    public boolean isChaotic() {
+        return this.category == Category.CHAOTIC;
+    }
+
+    public boolean isStrongerThan(Megabool other) {
+        return this.power > other.power;
+    }
+
+    public int getPower() {
+        return this.power;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
 }
